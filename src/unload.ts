8d391c5f8540e39e149f2c3ebb8a67b7150c62b0
WMXPY <wm@mengw.io>
2020-04-29 12:25:30 -0500

    fix implement unload listener

--- src/unload.ts
+++ src/unload.ts
@@ -15,18 +15,68 @@ export class UnloadListener {
 
     private readonly _listener: ListenerFunction;
 
+    private _activated: boolean;
+
     private constructor(listener: ListenerFunction) {
 
         this._listener = listener;
+        this._activated = false;
+
+        this.active = this.active.bind(this);
+        this.release = this.release.bind(this);
+    }
+
+    public get listening(): boolean {
+        return this._activated;
+    }
+
+    public attemptActive(): this {
+
+        if (this._activated) {
+            return this;
+        }
+
+        return this._activeListener();
     }
 
     public active() {
 
+        if (this._activated) {
+            throw new Error('[BWNL-Listener] This Unload Listener is already activated');
+        }
 
+        return this._activeListener();
     }
 
-    private _activeListener() {
+    public attemptRelease(): this {
 
+        if (!this._activated) {
+            return this;
+        }
+
+        return this._releaseListener();
+    }
+
+    public release(): this {
+
+        if (!this._activated) {
+            throw new Error('[BWNL-Listener] Nothing To Release');
+        }
+
+        return this._releaseListener();
+    }
+
+    private _activeListener(): this {
+
+        this._activated = true;
         window.addEventListener('unload', this._listener);
+        return this;
+    }
+
+    private _releaseListener(): this {
+
+        this._activated = false;
+        window.removeEventListener('unload', this._listener);
+        return this;
     }
 }
