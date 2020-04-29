5d2d57a6ecb781207762690784acf78c5fef0f6b
WMXPY <wm@mengw.io>
2020-04-29 12:19:49 -0500

    init unload listener

---
+++ src/unload.ts
@@ -0,0 +1,17 @@
+/**
+ * @author WMXPY
+ * @namespace Listener
+ * @description Unload
+ */
+
+export class UnloadListener {
+
+    public static create(): UnloadListener {
+
+        return new UnloadListener();
+    }
+
+    private constructor() {
+
+    }
+}
